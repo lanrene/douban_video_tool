ce3384bba429a65a9cf752ebb59896ead11dba4c
G-yanpeng <liuyanpeng0707@gmail.com>
2021-10-18 18:00:06 +0800

    变更版本

--- db_search_tool.js
+++ db_search_tool.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         豆瓣电影划词搜索助手
-// @version      0.1.5
+// @version      0.1.6
 // @namespace    https://github.com/lanrene/douban_video_tool
 // @description  通过滑动选中视频名词搜索豆瓣信息。脚本根据@Johnny Li[网页搜索助手]修改
 // @icon         https://img3.doubanio.com/f/movie/d59b2715fdea4968a450ee5f6c95c7d7a2030065/pics/movie/apple-touch-icon.png
